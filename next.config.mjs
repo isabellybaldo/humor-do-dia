ce5ce76f88581ebfe6e2e129b884460d2219f085
isoca <baldoisabelly@gmail.com>
2025-08-03 15:57:59 -0300

    :rocket: Few fixes for localhost processes and github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 const repo = 'humor-do-dia';
+const isProd = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: `/${repo}/`,
-  basePath: `/${repo}`,
-  output: 'export',
+  assetPrefix: isProd ? `/${repo}/` : '',
+  basePath: isProd ? `/${repo}` : '',
+  output: isProd ? 'export' : undefined,
   reactStrictMode: true
 };
 
