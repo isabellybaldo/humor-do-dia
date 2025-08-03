7b25870446407ffaf36a31e9a76130e51cac575f
isoca <baldoisabelly@gmail.com>
2025-08-03 15:41:41 -0300

    :rocket: Update next.config.mjs for static export with asset prefix and base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+const repo = 'humor-do-dia';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  assetPrefix: `/${repo}/`,
+  basePath: `/${repo}`,
+  output: 'export',
+  reactStrictMode: true
+};
 
 export default nextConfig;
