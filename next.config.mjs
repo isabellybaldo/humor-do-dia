15d57e13516ec09e6370a180e7203499ac198437
Isabelly Baldo <baldoisabelly@gmail.com>
2026-09-20 04:13:38 -0300

    chore: add prisma, vitest, ts tooling and standalone build

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
-const repo = 'humor-do-dia';
-const isProd = process.env.NODE_ENV === 'production';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: isProd ? `/${repo}/` : '',
-  basePath: isProd ? `/${repo}` : '',
-  output: isProd ? 'export' : undefined,
-  reactStrictMode: true
+  output: 'standalone',
+  reactStrictMode: true,
 };
 
 export default nextConfig;
